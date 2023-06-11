8ff8e237fe4e8a26802f642d1fc483869ab41044
Vladimir Dubecky <dubecky.vd@gmail.com>
2023-06-12 01:16:12 +0200

    feat(backend): user model validation messages

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,18 +1,18 @@
 import z from "zod";
 
 export const UserLoginSchema = z.object({
-    hashedPassword: z.string(),
-    email: z.string().nonempty("asd5"),
+    hashedPassword: z.string({required_error: 'Missing `password` parameter'}).nonempty(),
+    email: z.string({required_error: 'Missing `email` parameter'}).nonempty(),
 })
 
 export const UserRegisterSchema = z.object({
-    firstName: z.string(),
-    lastName: z.string(),
-    phoneNumber: z.string(),
+    firstName: z.string({ required_error: 'Missing `firstName` parameter'}).nonempty(),
+    lastName: z.string({ required_error: 'Missing `lastName` parameter'}).nonempty(),
+    phoneNumber: z.string({required_error: 'Missing `phoneNumber` parameter'}).nonempty(),
 }).merge(UserLoginSchema);
 
 export const UserSchema = z.object({
-    id: z.string()
+    id: z.string().nonempty()
 }).merge(UserRegisterSchema);
 
 export type UserRegister = z.infer<typeof UserRegisterSchema>;
